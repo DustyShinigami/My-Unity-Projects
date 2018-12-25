bbb289dc1c578a2c8cc0ab0c9f2805c8546b34f1
DustyShinigami <t_smith5@hotmail.com>
2018-12-25 13:09:03 +0000

    Life's Checkpoints Upload
    My first commit

---
+++ Life's Checkpoints/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Checkpoint : MonoBehaviour
+{
+    public HealthManager theHealthManager;
+    public Renderer cpRenderer;
+    public Renderer postRenderer;
+    public SpriteRenderer pcRenderer;
+    public Material cpOff;
+    public Material cpOn;
+    public Material postOff;
+    public Material postOn;
+    //public int panelID = 0;
+    public GameObject[] infoPanels;
+
+    private bool _isInCheckpoint1Area;
+
+    void Start()
+    {
+        theHealthManager = FindObjectOfType<HealthManager>();
+        //infoPanels = GameObject.FindGameObjectsWithTag("InformationPanel");
+        //pCPanel = GameObject.Find("PC Panel");
+        //infoPanels = GameObject.FindGameObjectsWithTag("Info Panel");
+        //SwitchToPanel(panelID);
+        //theGameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void Checkpoint1On()
+    {
+        if (GameManager.currentGold >= 5)
+        {
+            cpRenderer.material = cpOn;
+            postRenderer.material = postOn;
+            pcRenderer.color = new Color(1f, 1f, 1f, 1f);
+            if (Input.GetKey(KeyCode.Return))
+            {
+                if (Input.GetKey(KeyCode.Space)) //&& !infoPanels[0].activeInHierarchy)
+                {
+                    Debug.Log("Panel Active");
+                    infoPanels[0].SetActive(true);
+                }
+                else if (Input.GetKey(KeyCode.Space)) //&& infoPanels[0].activeInHierarchy)
+                {
+                    Debug.Log("Panel Deactivated");
+                    infoPanels[0].SetActive(false);
+                }
+            }
+            /*if (Input.GetKeyDown(KeyCode.Space))
+            {
+                theGameManager.Panel1();
+            }*/
+            //gamingPC.transform.RotateAround(transform.position, transform.up, Time.deltaTime);
+            //gamingPC.transform.rotation = Quaternion.Slerp(angle, 0f, rotationSpeed * Time.deltaTime);
+        }
+        /*
+        //[] makes a variable an Array (a list). The 'foreach' loop will check through all the Checkpoint objects
+        Checkpoint[] checkpoints = FindObjectsOfType<Checkpoint>();
+        //For each Checkpoint Array called 'checkpoints', look for 'cp' and turn the others in the list off
+        foreach (Checkpoint cp in checkpoints)
+        {
+            cp.CheckpointOff();
+        }
+        theRenderer.material = cpOn;*/
+    }
+
+    /*public void Checkpoint1Info()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            theGameManager.Panel1();
+        }
+    }*/
+
+    public void Checkpoint1Off()
+    {
+        cpRenderer.material = cpOff;
+        postRenderer.material = postOff;
+        pcRenderer.color = new Color(1f, 1f, 1f, 5f);
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _isInCheckpoint1Area = true;
+        }
+
+        if (Input.GetKey(KeyCode.Return))
+        {
+            theHealthManager.SetSpawnPoint(transform.position);
+            Checkpoint1On();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _isInCheckpoint1Area = false;
+        }
+    }
+}
---
+++ Life's Checkpoints/Assets/Scripts/GoldPickup.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldPickup : MonoBehaviour {
+
+    public int value;
+    public GameObject goldBar;
+    public GameObject pickupEffect;
+    public HealthManager healthManager;
+
+    private Quaternion goldPosition;
+    private Vector3 goldRespawnPoint;
+
+    //private Coroutine _respawnCoroutine;
+
+    void Start()
+    //To reference a method from another script in a public function, an object reference must be used. In this case, 'checkpoint'.
+    //Reference a method from another script using GetComponent.
+    {
+        healthManager = GetComponent<HealthManager>();
+        goldRespawnPoint = goldBar.transform.position;
+        goldPosition = goldBar.transform.rotation;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag ("Player"))
+        {
+            FindObjectOfType<GameManager>().AddGold(value);
+            Instantiate(pickupEffect, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+    public void GoldReset()
+    {
+        if (healthManager.isRespawning == true)
+        {
+            Destroy(goldBar);
+            Instantiate(goldBar, goldRespawnPoint, goldPosition);
+            goldBar.transform.position = goldRespawnPoint;
+            goldBar.transform.rotation = goldPosition;
+        }
+    }
+}
