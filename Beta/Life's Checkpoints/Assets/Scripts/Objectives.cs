a5ac29261016642bb25afc38abaf017f0dade06d
DustyShinigami <t_smith5@hotmail.com>
2019-01-06 08:29:23 +0000

    commit

--- Beta/Life's Checkpoints/Assets/Scripts/Objectives.cs
+++ Beta/Life's Checkpoints/Assets/Scripts/Objectives.cs
@@ -7,7 +7,7 @@ public class Objectives : MonoBehaviour {
 
     public Text objective1;
     public Text checkpoint1Unlock;
-    public Text checkpointPrompt;
+    //public Text checkpointPrompt;
     public Text panelPrompt;
 
     void Start()
@@ -15,7 +15,7 @@ public class Objectives : MonoBehaviour {
         checkpoint1Unlock.enabled = false;
         objective1.text = "Collect 5 Gold Bars";
         Invoke("Hide", 3f);
-        checkpointPrompt.enabled = false;
+        //checkpointPrompt.enabled = false;
     }
 
     public void Objective1()
@@ -25,15 +25,15 @@ public class Objectives : MonoBehaviour {
             checkpoint1Unlock.enabled = true;
             checkpoint1Unlock.text = "Checkpoint Unlocked";
             Invoke("Hide", 3f);
-            ObjectiveComplete();
+            //ObjectiveComplete();
         }
     }
 
-    public void ObjectiveComplete()
+    /*public void ObjectiveComplete()
     {
         checkpointPrompt.enabled = true;
         checkpointPrompt.text = "Press Return to activate";
-    }
+    }*/
 
     void Hide()
     {
