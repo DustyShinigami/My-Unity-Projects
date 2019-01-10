4cafb459633b9874e26b9bbefbb0f9419d9df85c
DustyShinigami <t_smith5@hotmail.com>
2019-01-10 16:04:25 +0000

    commit

---
+++ Beta/2D Platformer/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    public GameObject objectToMove;
+    public Transform startPosition;
+    public Transform endPosition;
+    public float moveSpeed;
+
+    private Vector2 currentTarget;
+
+    //The platform needs to move from start to end on Play.
+    void Start()
+    {
+        currentTarget = endPosition.position;
+    }
+
+    void Update()
+    {
+        //Whether Vector 2 or 3, a MoveTowards requires a current position, a target position and a float
+        //Movement speeds must be multiplied by Time.deltaTime
+        objectToMove.transform.position = Vector2.MoveTowards(objectToMove.transform.position, currentTarget, moveSpeed * Time.deltaTime);
+        if(objectToMove.transform.position == endPosition.position)
+        {
+            currentTarget = startPosition.position;
+        }
+        if(objectToMove.transform.position == startPosition.position)
+        {
+            currentTarget = endPosition.position;
+        }
+    }
+}
