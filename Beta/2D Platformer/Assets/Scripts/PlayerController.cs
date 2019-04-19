90030e36504e4b50955a20d566525e966b1b9585
DustyShinigami <t_smith5@hotmail.com>
2019-04-19 02:29:49 +0100

    commit

---
+++ Beta/2D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public bool isGrounded;
+    public Vector3 respawnPosition;
+    public LevelManager theLevelManager;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        respawnPosition = transform.position;
+        theLevelManager = FindObjectOfType<LevelManager>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            transform.localScale = new Vector2(1f, 1f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+            transform.localScale = new Vector2(-1f, 1f);
+        }
+        //Stops the player character from sliding when they stop.
+        else
+        {
+            rb.velocity = new Vector2(0f, rb.velocity.y);
+        }
+
+        //If the jump key is pressed and the player is grounded, they jump. If they're not grounded, the jump key can't be pressed. This stops the player from being able to keep jumping.
+        if (Input.GetKey(KeyCode.KeypadPlus) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        //Mathf.Abs (Abs is short for Absolute) makes a minus number into a whole number - eg: -5 to 5
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        anim.SetBool("Grounded", isGrounded);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "KillPlane")
+        {
+            theLevelManager.Respawn();
+        }
+        if(other.tag == "Checkpoint")
+        {
+            respawnPosition = transform.position;
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D other)
+    {
+        if(other.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = other.gameObject.transform;
+        }
+    }
+
+    void OnCollisionExit2D (Collision2D other)
+    {
+        if(other.gameObject.tag == "MovingPlatform")
+        {
+            transform.parent = null;
+        }
+    }
+}
---
+++ Beta/Life's Checkpoints/Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour
+{
+    public int damnageToGive = 10;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Vector2 hitDirection = other.transform.position - transform.position;
+            hitDirection = hitDirection.normalized;
+            FindObjectOfType<HealthManager>().HurtPlayer(damnageToGive, hitDirection);
+        }
+
+    }
+}
