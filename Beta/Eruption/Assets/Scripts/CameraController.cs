82ab0ff59683cc05a5b3a5bd1f0c323018f1dc19
DustyShinigami <t_smith5@hotmail.com>
2019-04-22 02:59:48 +0100

    commit

---
+++ Beta/Eruption/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] GameObject player = null;
+    [SerializeField] [Range(0.1f, 3f)] float followAhead = 2f;
+    [SerializeField] [Range(0.1f, 3f)] float smoothing = 1f;
+
+    const float m_minY = 2f;
+    Vector3 targetPosition;
+    Vector3 cameraOffset;
+
+    void Start()
+    {
+        cameraOffset = transform.position - player.transform.position;
+    }
+
+    private void Update()
+    {
+        targetPosition = (player.transform.position + (player.transform.forward * followAhead)) + cameraOffset;
+        targetPosition.y = Mathf.Min(targetPosition.y, m_minY);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+    }
+}
