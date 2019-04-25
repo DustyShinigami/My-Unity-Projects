32f12c5b6d2615f25e9917a20fb9993efdba2f51
DustyShinigami <t_smith5@hotmail.com>
2019-04-25 17:49:47 +0100

    commit

--- Beta/Eruption/Assets/Scripts/CameraController.cs
+++ Beta/Eruption/Assets/Scripts/CameraController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] GameObject player = null;
-    [SerializeField] [Range(0.1f, 2f)] float followAhead = 2f;
-    [SerializeField] [Range(0.1f, 2f)] float smoothing = 1f;
+    //[SerializeField] [Range(0.1f, 2f)] float followAhead = 2f;
+    //[SerializeField] [Range(0.1f, 2f)] float smoothing = 1f;
 
-    //public PlayerController playerController;
+    public BoxCollider boundBox;
 
-    //private float minPosition = -9.37f;
-   // private float maxPosition = 4.68f;
-    private Vector3 cameraClamp;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
 
     const float m_minY = 2f;
     Vector3 targetPosition;
@@ -20,17 +22,24 @@ public class CameraController : MonoBehaviour
 
     void Start()
     {
+        theCamera = GetComponent<Camera>();
         cameraOffset = transform.position - player.transform.position;
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
     }
 
     void Update()
     {
-        targetPosition = (player.transform.position + (player.transform.forward * followAhead)) + cameraOffset;
+        //targetPosition = (player.transform.position + (player.transform.forward * followAhead)) + cameraOffset;
         //smoothedForward = Vector3.MoveTowards(smoothedForward, playerController.GetTravelDirection(), 0.5f * Time.deltaTime);
         //targetPosition = (player.transform.position + (smoothedForward * followAhead));
-        //transform.position = player.transform.position + cameraOffset;
+        transform.position = player.transform.position + cameraOffset;
         targetPosition.y = Mathf.Min(targetPosition.y, m_minY);
-        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
-        cameraClamp.x = Mathf.Clamp(transform.position.x, -9.37f, 4.68f);
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime);
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 }
