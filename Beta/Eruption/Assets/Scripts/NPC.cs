6704fe1d2a18802fa4eb8cecf539394eba8b74fa
DustyShinigami <t_smith5@hotmail.com>
2019-05-19 19:14:48 +0100

    commit

--- Beta/Eruption/Assets/Scripts/NPC.cs
+++ Beta/Eruption/Assets/Scripts/NPC.cs
@@ -5,52 +5,112 @@ using UnityEngine.SceneManagement;
 
 public class NPC : MonoBehaviour
 {
+    public Animator anim;
     public float moveSpeed;
     public bool isWalking;
     public float walkTime;
+    public float gravityScale;
+    public bool canMove;
+    public static bool interact = false;
+    public static bool allowInteract = false;
+    public GameObject destinationTarget;
+    public GameObject startPoint;
+    public float targetDistance;
+    public float allowedDistance = 5;
+    public RaycastHit shot;
 
+    private Vector3 moveDirection;
+    private Vector3 extraDirections;
     private float walkCounter;
-    private Rigidbody2D myRigidbody;
     private int WalkDirection;
+    private CharacterController controller;
 
     void Start()
     {
-        myRigidbody = GetComponent<Rigidbody2D>();
+        controller = GetComponent<CharacterController>();
+        canMove = false;
         walkCounter = walkTime;
-        MoveDirection();
+        //MoveDirection();
     }
 
     public void Update()
     {
-        if (InteractionController.charactersTalking)
+        if (!canMove)
         {
-            isWalking = true;
+            isWalking = false;
+            anim.SetBool("isGrounded", controller.isGrounded);
+            //If the character can't move, then the Speed is set to 0. Otherwise it'll use the horizontal input value.
+            anim.SetFloat("Speed",
+                !canMove
+                ? 0f
+                : Mathf.Abs(Input.GetAxis("Horizontal")));
         }
-        if (isWalking)
+
+        if (canMove)
         {
-            walkCounter -= Time.deltaTime;
-            if(walkCounter < 0)
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            moveDirection = new Vector2(moveHorizontal * moveSpeed, moveDirection.y);
+            extraDirections = new Vector2(moveVertical * moveSpeed, extraDirections.y);
+            controller.Move(moveDirection * Time.deltaTime);
+
+            if (moveHorizontal > 0)
             {
-                isWalking = false;
+                transform.eulerAngles = new Vector2(0, 90);
             }
-
-            switch (WalkDirection)
+            else if (moveHorizontal < 0)
             {
-                case 0:
-                    myRigidbody.velocity = new Vector2(moveSpeed, 0);
-                    break;
+                transform.eulerAngles = new Vector2(0, -90);
+            }
+            if (!InteractionController.charactersTalking)
+            {
+                isWalking = true;
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out shot))
+                {
+                    targetDistance = shot.distance;
+                    if (targetDistance >= allowedDistance)
+                    {
+                        moveSpeed = 0.02f;
+                        GetComponent<Animation>().Play("Walk");
+                        transform.position = Vector3.MoveTowards(startPoint.transform.position, destinationTarget.transform.position, moveSpeed);
+                    }
+                    else
+                    {
+                        moveSpeed = 0;
+                        GetComponent<Animation>().Play("Idle");
+                    }
+                }
 
-                case 1:
-                    myRigidbody.velocity = new Vector2(-moveSpeed, 0);
-                    break;
+                if (interact)
+                {
+                    anim.SetBool("Interact", controller.isGrounded);
+                }
             }
         }
-    }
+            /*if (isWalking)
+            {
+                walkCounter -= Time.deltaTime;
+                if (walkCounter < 0)
+                {
+                    isWalking = false;
+                }
+                /*switch (WalkDirection)
+                {
+                    case 0:
+                        controller.velocity = new Vector2(moveSpeed, 0);
+                        break;
+
+                    case 1:
+                        myRigidbody.velocity = new Vector2(-moveSpeed, 0);
+                        break;
+                }
+            }
 
     public void MoveDirection()
     {
         WalkDirection = Random.Range(0, 1);
         isWalking = true;
         walkCounter = walkTime;
+    }*/
     }
 }
