83100f1d869a11aa34bd914a0a58724f1f9001eb
DustyShinigami <t_smith5@hotmail.com>
2019-05-28 19:23:42 +0100

    commit

---
+++ Beta/Eruption/Assets/Scripts/LoadNewArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewArea : MonoBehaviour
+{
+    public string exitPoint;
+
+    private PlayerController thePlayer;
+    private PlayerStartPoints startPoints;
+
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+        startPoints = FindObjectOfType<PlayerStartPoints>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag ("Player"))
+        {
+            thePlayer.startPoint = exitPoint;
+            startPoints.NewArea();
+        }
+    }
+}
--- Beta/Eruption/Assets/Scripts/PlayerStartPoints.cs
+++ Beta/Eruption/Assets/Scripts/PlayerStartPoints.cs
@@ -4,23 +4,25 @@ using UnityEngine;
 
 public class PlayerStartPoints : MonoBehaviour
 {
-    //public Vector3 startDirection;
-    //public string pointName;
+    public string pointName;
 
     private PlayerController thePlayer;
-    private CameraController theCamera;
+    //private CameraController theCamera;
 
     void Start()
     {
         thePlayer = FindObjectOfType<PlayerController>();
-
         //The player(controller's) startPoint, which has been accessed and the variable found on Start, checks to see if it's equal to or matches the pointName, which is found on each Start Point object through this script. If it does match, the player's transform/position will match the Start Point's. If it doesn't match, the player will be placed in the default location, such as at the beginning of the start_area scene. The pointName for a building's entrance/exit in one scene must match those in another scene for this to work! Think of them as teleporter links.
-        /*if (thePlayer.startPoint == pointName)
+        //thePlayer.transform.position = transform.position;
+        /*theCamera = FindObjectOfType<CameraController>();
+        theCamera.transform.position = new Vector3(theCamera.transform.position.x, theCamera.transform.position.y, transform.transform.position.z);*/
+    }
+
+    public void NewArea()
+    {
+        if (thePlayer.startPoint == pointName)
         {
             thePlayer.transform.position = transform.position;
-        }*/
-
-        theCamera = FindObjectOfType<CameraController>();
-        theCamera.transform.position = new Vector3(theCamera.transform.position.x, theCamera.transform.position.y, transform.transform.position.z);
+        }
     }
 }
