f5d13313e5803c1725080ee653a7deca7c907f11
DustyShinigami <t_smith5@hotmail.com>
2019-05-29 00:12:51 +0100

    commit

--- Beta/Eruption/Assets/Scripts/LoadNewArea.cs
+++ Beta/Eruption/Assets/Scripts/LoadNewArea.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadNewArea : MonoBehaviour
 {
-    public bool nextArea;
-    public Transform newStartPoint;
+    //public Transform newStartPoint;
 
     private PlayerController thePlayer;
     private ScreenFader theScreenFader;
-    private CameraController theCamera;
-    private bool playerMoved;
+    private SceneManagement theSceneManager;
+    private bool nextArea;
+    //private CameraController theCamera;
+    //private bool playerMoved;
 
     void Start()
     {
         thePlayer = FindObjectOfType<PlayerController>();
         theScreenFader = FindObjectOfType<ScreenFader>();
-        theCamera = FindObjectOfType<CameraController>();
+        theSceneManager = FindObjectOfType<SceneManagement>();
+        //theCamera = FindObjectOfType<CameraController>();
     }
 
     public void OnTriggerEnter(Collider other)
@@ -32,18 +35,18 @@ public class LoadNewArea : MonoBehaviour
     {
         thePlayer.gameObject.SetActive(false);
         theScreenFader.StartCoroutine("ScreenFade");
-        yield return new WaitForSeconds(3f);
-        thePlayer.transform.position = newStartPoint.position;
-        playerMoved = true;
-        thePlayer.gameObject.SetActive(true);
-        //theCamera.transform.position = new Vector3(14.4f, theCamera.transform.position.y, theCamera.transform.position.z);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("level 1, room 2");
+        //thePlayer.transform.position = newStartPoint.position;
+        //playerMoved = true;
+        //thePlayer.gameObject.SetActive(true);
     }
 
-    void Update()
+    /*void Update()
     {
         if (playerMoved)
         {
             theCamera.transform.position = new Vector3(14.4f, theCamera.transform.position.y, theCamera.transform.position.z);
         }
-    }
+    }*/
 }
