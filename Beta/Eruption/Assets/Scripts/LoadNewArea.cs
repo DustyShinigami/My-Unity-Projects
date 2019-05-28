c19700cf4ab238c8f86c35cdef56b44658ceb49b
DustyShinigami <t_smith5@hotmail.com>
2019-05-28 14:53:10 +0100

    commit

---
+++ Beta/Eruption/Assets/Scripts/LoadNewArea.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNewArea : MonoBehaviour
+{
+    //public GameObject exitPoint;
+    public string exitPoint;
+
+    private PlayerController thePlayer;
+
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            thePlayer.startPoint = exitPoint;
+        }
+    }
+}
---
+++ Beta/Eruption/Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenFader : MonoBehaviour
+{
+    public Image blackScreen;
+    public float fadeSpeed;
+    public float waitForFade;
+
+    private bool isFadetoBlack;
+    private bool isFadefromBlack;
+
+    void Start()
+    {
+        //DontDestroyOnLoad(this);
+        //DontDestroyOnLoad(blackScreen);
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("start_area"))
+        {
+            StartCoroutine("ScreenFade");
+        }
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("hut_interior"))
+        {
+            StartCoroutine("ScreenFade");
+        }
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("start_area 2"))
+        {
+            StartCoroutine("ScreenFade");
+        }
+    }
+
+    IEnumerator ScreenFade()
+    {
+        yield return new WaitForSeconds(fadeSpeed);
+        isFadetoBlack = true;
+        yield return new WaitForSeconds(waitForFade);
+        isFadefromBlack = true;
+    }
+
+    void Update()
+    {
+        if (isFadetoBlack)
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, Mathf.MoveTowards(blackScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
+            if (blackScreen.color.a == 1f)
+            {
+                isFadetoBlack = false;
+            }
+        }
+        if (isFadefromBlack)
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, Mathf.MoveTowards(blackScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+            if (blackScreen.color.a == 0f)
+            {
+                isFadefromBlack = true;
+            }
+        }
+    }
+}
