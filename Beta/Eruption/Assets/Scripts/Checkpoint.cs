aa9f4039f41ccc6bfa266468bb5fb353409321ce
DustyShinigami <t_smith5@hotmail.com>
2019-06-01 00:46:58 +0100

    commit

---
+++ Beta/Eruption/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Sprite litCandle;
+
+    void Start()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
