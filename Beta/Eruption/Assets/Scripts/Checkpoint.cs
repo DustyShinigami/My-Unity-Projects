c0f295c297539dcba267a9cbbb393c7d8e1bfa35
DustyShinigami <t_smith5@hotmail.com>
2019-06-01 01:34:33 +0100

    commit
    - Added script and trigger that changes the candle sprite to lit when it's activated

--- Beta/Eruption/Assets/Scripts/Checkpoint.cs
+++ Beta/Eruption/Assets/Scripts/Checkpoint.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    public Sprite unlitCandle;
-    public Sprite litCandle;
+    public GameObject unlitCandle;
+    public GameObject litCandle;
 
-    private SpriteRenderer theSpriteRenderer;
     private bool checkpointActive;
-
-    void Start()
-    {
-        theSpriteRenderer = GetComponent<SpriteRenderer>();
-    }
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == "Player")
         {
-            theSpriteRenderer.sprite = litCandle;
+            unlitCandle.gameObject.SetActive(false);
+            litCandle.gameObject.SetActive(true);
             checkpointActive = true;
         }
     }
