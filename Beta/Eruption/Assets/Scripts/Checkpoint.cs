ae8ef4c3b2a586f4d5716c7e09ad2e54baa02155
DustyShinigami <t_smith5@hotmail.com>
2019-06-01 01:00:14 +0100

    commit

--- Beta/Eruption/Assets/Scripts/Checkpoint.cs
+++ Beta/Eruption/Assets/Scripts/Checkpoint.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    public Sprite unlitCandle;
     public Sprite litCandle;
+
+    private SpriteRenderer theSpriteRenderer;
+    private bool checkpointActive;
 
     void Start()
     {
-        
+        theSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.name == "Player")
         {
+            theSpriteRenderer.sprite = litCandle;
+            checkpointActive = true;
         }
-    }
-
-    void Update()
-    {
-        
     }
 }
