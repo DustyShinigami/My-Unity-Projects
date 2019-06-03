66006bcbc6096b626b00c8ceeb096addb895c65d
DustyShinigami <t_smith5@hotmail.com>
2019-06-03 19:13:58 +0100

    commit

---
+++ Beta/Eruption/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    /*public PlayerController thePlayer;
+    public float reloadLength;
+
+    private Vector3 respawnPoint;
+    private bool playerReloading;
+
+    void Start()
+    {
+        respawnPoint = thePlayer.transform.position;
+        /*Player player = new Player();
+        player.SetPosition(new Vector3(0, 0, 0));
+        player.SetRotation(new Vector3(0, 0, 0));
+        player.SetControl(true);
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void PlayerReload()
+    {
+        if (playerReloading)
+        {
+            StartCoroutine("ReloadingCo");
+        }
+    }
+
+    public IEnumerator ReloadingCo()
+    {
+        Instantiate(thePlayer, thePlayer.transform.position, thePlayer.transform.rotation);
+        yield return new WaitForSeconds(reloadLength);
+        playerReloading = false;
+        thePlayer.transform.position = respawnPoint;
+        Debug.Log("Player respawned");
+    }
+
+    public void SetSpawnPoint(Vector3 newPosition)
+    {
+        respawnPoint = newPosition;
+    }*/
+}
---
+++ Beta/Eruption/Assets/Scripts/LoadNewArea.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNewArea : MonoBehaviour
+{
+    //public Transform newStartPoint;
+
+    private PlayerController thePlayer;
+    private ScreenFader theScreenFader;
+    private SceneManagement theSceneManager;
+    public bool nextArea;
+    //private CameraController theCamera;
+    //private bool playerMoved;
+
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PlayerController>();
+        theScreenFader = FindObjectOfType<ScreenFader>();
+        theSceneManager = FindObjectOfType<SceneManagement>();
+        //theCamera = FindObjectOfType<CameraController>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag ("Player"))
+        {
+            nextArea = true;
+            StartCoroutine("NextArea");
+        }
+    }
+
+    IEnumerator NextArea()
+    {
+        if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("start_area 2"))
+        {
+            thePlayer.gameObject.SetActive(false);
+            theScreenFader.StartCoroutine("ScreenFade");
+            yield return new WaitForSeconds(2f);
+            SceneManager.LoadScene("level 1, room 1");
+        }
+        else if(SceneManager.GetActiveScene() == SceneManager.GetSceneByName("level 1, room 1"))
+        {
+            thePlayer.gameObject.SetActive(false);
+            theScreenFader.StartCoroutine("ScreenFade");
+            yield return new WaitForSeconds(2f);
+            SceneManager.LoadScene("level 1, room 2");
+        }
+        //thePlayer.transform.position = newStartPoint.position;
+        //playerMoved = true;
+        //thePlayer.gameObject.SetActive(true);
+    }
+
+    /*void Update()
+    {
+        if (playerMoved)
+        {
+            theCamera.transform.position = new Vector3(14.4f, theCamera.transform.position.y, theCamera.transform.position.z);
+        }
+    }*/
+}
