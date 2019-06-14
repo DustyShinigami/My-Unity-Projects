87a3401db2ce5ad5293225117ce63d168c1a595a
DustyShinigami <t_smith5@hotmail.com>
2019-06-14 17:21:10 +0100

    commit
    + Added Secret text that pops up when the player picks up a secret and then it disappears
    
    - Need to figure out how to move the text down and back up after 2 seconds

--- Beta/Eruption/Assets/Scripts/GameManager.cs
+++ Beta/Eruption/Assets/Scripts/GameManager.cs
@@ -7,11 +7,40 @@ using TMPro;
 public class GameManager : MonoBehaviour {
 
     public static int currentEmbers;
+    public static int currentSecrets;
     public TextMeshProUGUI emberText;
+    public TextMeshProUGUI secretsText;
+    public bool secretCollected = false;
+    public float timer = 2f;
+
+    void Start()
+    {
+        secretsText.enabled = false;
+    }
+
+    void Update()
+    {
+        if (secretCollected)
+        {
+            secretsText.enabled = true;
+            timer -= Time.deltaTime;
+            if (timer <= 0f)
+            {
+                secretsText.enabled = false;
+            }
+        }
+    }
 
     public void AddEmber(int embertoAdd)
     {
         currentEmbers += embertoAdd;
         emberText.text = "Embers: " + currentEmbers;
+    }
+
+    public void AddSecret(int secrettoAdd)
+    {
+        secretCollected = true;
+        currentSecrets += secrettoAdd;
+        secretsText.text = "Secrets Found: " + currentSecrets;
     }
 }
---
+++ Beta/Eruption/Assets/Scripts/SecretsPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretsPickup : MonoBehaviour
+{
+    public int value;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().AddSecret(value);
+            Destroy(gameObject);
+        }
+    }
+}
