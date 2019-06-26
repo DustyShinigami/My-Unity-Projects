4a6cc766f888af9096fa7a9aeb634db04e8a870d
DustyShinigami <t_smith5@hotmail.com>
2019-06-26 09:23:41 +0100

    commit

---
+++ Beta/Eruption/Assets/Scripts/GunScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject projectile;
+    public float projectileSpeed;
+
+    public void Start()
+    {
+        projectile.SetActive(false);
+    }
+
+    public void Fire()
+    {
+        projectile.SetActive(true);
+    }
+
+}
