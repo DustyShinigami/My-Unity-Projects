bd8a1b684150751736d85bb882b085007d82c779
DustyShinigami <t_smith5@hotmail.com>
2019-07-03 12:05:50 +0100

    commit

---
+++ Beta/Eruption/Assets/Scripts/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    public static UI instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    private static UI _instance;
+
+    public static void Init()
+    {
+        if (_instance = null) SceneManager.LoadScene("level 1, room 1", LoadSceneMode.Additive);
+    }
+
+    void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
